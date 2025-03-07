c11dffd4235d545369d5ef000b438cdf29f03325
joshlian <joshuatzlian@gmail.com>
2025-03-07 03:47:50 +0000

    second commit

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -1,5 +1,8 @@
 package Controller;
 
+import Model.Account;
+import Service.AccountService;
+
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
@@ -9,25 +12,54 @@ import io.javalin.http.Context;
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
 public class SocialMediaController {
-    /**
-     * In order for the test cases to work, you will need to write the endpoints in the startAPI() method, as the test
-     * suite must receive a Javalin object from this method.
+
+    AccountService accountService;
+    public SocialMediaController()
+    {
+        accountService = new AccountService();
+    }
+
+    /*
      * @return a Javalin app object which defines the behavior of the Javalin controller.
      */
     public Javalin startAPI() {
         Javalin app = Javalin.create();
-        app.get("example-endpoint", this::exampleHandler);
+        app.post("/register", this::registerUser);
+        app.post("/login", this::loginUser);
 
         return app;
     }
 
-    /**
-     * This is an example handler for an example endpoint.
-     * @param context The Javalin Context object manages information about both the HTTP request and response.
+    /*
+     * @param ctx The Javalin Context object manages information about both the HTTP request and response.
      */
-    private void exampleHandler(Context context) {
-        context.json("sample text");
+
+    /* registering users */
+    private void registerUser(Context ctx) {
+        Account account = ctx.bodyAsClass(Account.class);
+
+        Account createdAccount = accountService.register(account);
+
+        if (createdAccount == null) 
+        {
+            ctx.status(400);
+        } else 
+        {
+            ctx.json(createdAccount);
+        }
     }
 
+    /* login users */
+    private void loginUser(Context ctx) {
+        Account loginRequest = ctx.bodyAsClass(Account.class);
+        Account account = accountService.login(loginRequest.getUsername(), loginRequest.getPassword());
 
+        if (account == null) 
+        {
+            ctx.status(401);
+        } else 
+        {
+            ctx.json(account);
+        }
+    }
 }
